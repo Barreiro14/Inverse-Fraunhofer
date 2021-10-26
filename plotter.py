d020377bbc8eb5f92eb76552a7eecc8764729c0c
Gabriel Barreiro <elbarri@omar-pc>
2021-10-26 11:08:27 -0400

    fix plotter

--- plotter.py
+++ plotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plotter(arr):
     i = 0
@@ -6,8 +7,13 @@ def plotter(arr):
     while i < len(arr):
         while j < len(arr[0]):
             if arr[i][j] != 0:
-                plt.plot(i, j)
+                plt.plot(i, j, 'bo')
             j = j +1
         i = i + 1
     plt.show()
 
+arr = np.array([[0,1,1], [1, 0, 0], [1, 0, 1]])
+print(len(arr[0]))
+plotter(arr)
+#plt.plot(1, 2, 'bo')
+#plt.show()
